b2b20e1a51600c4f2c89d06dd99873b7d99c5f7c
dawijaya <113819824+dawijaya@users.noreply.github.com>
2023-11-26 10:01:06 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,51 @@
 // script.js
 document.addEventListener('DOMContentLoaded', function() {
-    // Fungsi untuk memulai backsound secara otomatis
+    const backsound = document.getElementById('backsound');
+    const button = document.querySelector('button');
+
     function playBacksound() {
-        const backsound = document.getElementById('backsound');
-        backsound.play();
+        backsound.play().then(function() {
+            // Musik dimainkan
+        }).catch(function(error) {
+            // Autoplay tidak diizinkan, tidak perlu melakukan apa-apa
+        });
     }
 
     // Panggil fungsi untuk memulai backsound saat halaman dimuat
     playBacksound();
+
+    // Event listener untuk memulai backsound saat dokumen diklik
+    document.addEventListener('click', function() {
+        playBacksound();
+        // Hapus event listener setelah interaksi pertama kali
+        document.removeEventListener('click', arguments.callee);
+
+        // Setelah klik, tambahkan event listener untuk menyembunyikan tombol setelah perintah selesai
+        document.addEventListener('click', function() {
+            hideButton();
+        });
+    });
 });
 
 function showMessage() {
     const messageDiv = document.getElementById('message');
-    const backsound = document.getElementById('backsound');
-
     const response = confirm("Maukah kamu menjalani kehidupan ini bersamaku?");
 
     if (response) {
         const answer = confirm("Maukah kamu menjadi pacarku?");
         if (answer) {
-            messageDiv.innerHTML = "terimakasih sayang.";
+            // Mengubah warna teks menjadi merah, contoh
+            messageDiv.innerHTML = "<span style='color: white;'>Terimakasih sayang, Sekarang lihat aku.</span>";
         } else {
             messageDiv.innerHTML = "Terimakasih Siti.";
         }
     } else {
         messageDiv.innerHTML = "Program diakhiri.";
     }
+}
+
+
+function hideButton() {
+    const button = document.querySelector('button');
+    button.style.display = 'none';
 }
